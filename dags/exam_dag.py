691d0e8bd52e40ccd8be2a23c8867e18d4b1658a
Ubuntu <ubuntu@ip-172-31-28-131.eu-west-1.compute.internal>
2025-04-29 17:15:44 +0000

    add new tasks to exam_dag

--- dags/exam_dag.py
+++ dags/exam_dag.py
@@ -6,28 +6,60 @@ import datetime
 import requests
 import os
 import json
+import pandas as pd
 
-def request_api(city_name):
-    url = "https://api.openweathermap.org/data/2.5/weather"
-    params = {
-    "q": city_name,
-    "appid": '89a03ead634e44c5afea080d6eaed1d7',
-    "units": "metric"  # pour des températures en °C
-    }
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
-        current_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-        os.makedirs('/opt/airflow/raw_files', exist_ok=True)
-        with open(f'/opt/airflow/raw_files/{city_name}_{current_datetime}.json', 'w') as f:
-            json.dump(response.json(), f)
-    else:
-        print(f"Erreur {response.status_code} : {response.text}")
+def request_api(city_names):
+    api_responses = []
+    for city_name in city_names:
+        url = "https://api.openweathermap.org/data/2.5/weather"
+        params = {
+        "q": city_name,
+        "appid": '89a03ead634e44c5afea080d6eaed1d7',
+        "units": "metric"  # pour des températures en °C
+        }
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            api_responses.append(response.json())
+        else:
+            print(f"Erreur {response.status_code} : {response.text}")
+    current_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+    os.makedirs('/app/raw_files', exist_ok=True)
+    with open(f'/app/raw_files/{current_datetime}.json', 'w') as f:
+        json.dump(api_responses, f)
+
+def transform_data_into_csv(n_files=None, filename='data.csv'):
+    parent_folder = '/app/raw_files'
+    files = sorted(os.listdir(parent_folder), reverse=True)
+    if n_files:
+        files = files[:n_files]
+    print()
+
+    dfs = []
+
+    for f in files:
+        with open(os.path.join(parent_folder, f), 'r') as file:
+            data_temp = json.load(file)
+        for data_city in data_temp:
+            dfs.append(
+                {
+                    'temperature': data_city['main']['temp'],
+                    'city': data_city['name'],
+                    'pression': data_city['main']['pressure'],
+                    'date': f.split('.')[0]
+                }
+            )
+
+    df = pd.DataFrame(dfs)
+
+    print('\n', df.head(10))
+    os.makedirs('/app/clean_data', exist_ok=True)
+    df.to_csv(os.path.join('/app/clean_data', filename), index=False)
 
 cities = json.loads(Variable.get("cities"))
 
 with DAG(
     dag_id='exam_dag',
-    schedule_interval=None,
+    schedule_interval=None, #datetime.timedelta(seconds=20)
     tags=['tutorial', 'datascientest'],
     default_args={
         'owner': 'airflow',
@@ -36,11 +68,23 @@ with DAG(
     catchup=False
 ) as my_dag:
 
-    api_request_tasks = []
-    for city in cities:
-        task = PythonOperator(
-            task_id=f'request_for_{city}',
+
+    api_request_task = PythonOperator(
+            task_id=f'api_request',
             python_callable=request_api,
-            op_kwargs={'city_name': city}
+            op_kwargs={'city_names': cities}
         )
-        api_request_tasks.append(task)
+
+    agg_data_task_dashboard = PythonOperator(
+            task_id=f'aggregate_data_dashboard',
+            python_callable=transform_data_into_csv,
+            op_kwargs={'n_files': 20}
+        )
+
+    agg_data_task_model = PythonOperator(
+            task_id=f'aggregate_data_model',
+            python_callable=transform_data_into_csv,
+            op_kwargs={'filename': 'fulldata.csv'}
+        )
+
+    api_request_task >> [agg_data_task_dashboard, agg_data_task_model]
