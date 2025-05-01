052b17d02c613d446aa97ffa7868b45ca5938932
Ubuntu <ubuntu@ip-172-31-28-131.eu-west-1.compute.internal>
2025-05-01 07:17:15 +0000

    add model tasks

--- dags/exam_dag.py
+++ dags/exam_dag.py
@@ -2,11 +2,15 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 from airflow.models import Variable
+from airflow.utils.task_group import TaskGroup
+from train_model import prepare_data, compute_model_score, train_and_save_model
 import datetime
 import requests
 import os
 import json
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
 
 def request_api(city_names):
     api_responses = []
@@ -57,6 +61,21 @@ def transform_data_into_csv(n_files=None, filename='data.csv'):
 
 cities = json.loads(Variable.get("cities"))
 
+def prepare_data_dict(path_to_data):
+    X, y = prepare_data(path_to_data)
+    return X.to_dict(), y.to_dict()
+
+def compute_model_score_from_xcom(task_instance, model, model_name):
+    X_dict, y_dict = task_instance.xcom_pull(task_ids='prepare_data')
+    X = pd.DataFrame(X_dict)
+    y = pd.DataFrame(y_dict)
+    task_instance.xcom_push(
+        key=f"{model_name}_result",
+        value=compute_model_score(X, y, model)
+    )
+
+
+
 with DAG(
     dag_id='exam_dag',
     schedule_interval=None, #datetime.timedelta(seconds=20)
@@ -86,5 +105,30 @@ with DAG(
             python_callable=transform_data_into_csv,
             op_kwargs={'filename': 'fulldata.csv'}
         )
+
+    with TaskGroup("model_tasks") as model_tasks:
+        prepare_data_task = PythonOperator(
+        task_id='prepare_data',
+        python_callable=prepare_data_dict,
+        )
+
+        compute_linear_regression_score = PythonOperator(
+        task_id='cross_val_linear',
+        python_callable=compute_model_score_from_xcom,
+        op_kwargs={
+            'model': LinearRegression(),
+            'model_name': 'linear_regression'
+        }
+        )
+
+        compute_decision_tree_regression_score = PythonOperator(
+        task_id='cross_val_decision_tree',
+        python_callable=compute_model_score_from_xcom,
+        op_kwargs={
+            'model': DecisionTreeRegressor(),
+            'model_name': 'decision_tree'
+        }
+        )
+        
 
     api_request_task >> [agg_data_task_dashboard, agg_data_task_model]
